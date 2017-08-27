bc6a6b7ae8e0381df4fbb6bbb069dbf68408aa60
James Thayer <jrthayer@users.noreply.github.com>
2017-08-26 18:12:04 -0700

    Add SDL libraries and header files to project files
    This allows the repo to work on any device it is downloaded on as long as the compiler and linker paths are set.
    
    These files include all the files needed for the 32bit version of SDL2, true type font, texture, and mixer extensions.
    
    *SDL libraries for linker = /SDL2/lib
    *SDL headers for compiler = /SDL2/include
    *project headers for compiler = /pHeaders

---
+++ pHeaders/gameEngine.h
@@ -0,0 +1,13 @@
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+class GameEngine
+{
+    public:
+        void run();
+        void close();
+
+    private:
+
+};
+#endif // GAMEENGINE_H
---
+++ pSources/gameEngine.cpp
@@ -0,0 +1,21 @@
+#include "gameEngine.h"
+
+void GameEngine::run()
+{
+    while(1)
+    {
+        //call event handler
+        //call game handler
+        //call texture handler
+    }
+
+    close();
+
+}
+
+void GameEngine::close()
+{
+    //destroy event handler
+    //destroy game handler
+    //destroy texture handler
+}
